70554a070ad017c4d706a3707375266d60077635
michaelzheng67 <michaelzheng67@yahoo.ca>
2022-01-15 15:06:01 -0500

    First commit

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'; 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Meat Ur Meat</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
